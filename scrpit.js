29a28e1c5fab0afee8d75fa9c9d5992a155ae46c
Suraj-agrahari <Tha075bei043@tcioe.edu.np>
2023-07-11 08:23:47 +0545

    hiding api

--- scrpit.js
+++ scrpit.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "sdahdsa"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
